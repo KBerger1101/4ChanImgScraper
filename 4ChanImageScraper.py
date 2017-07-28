99b796e5339b98c57941d91c81de5fb524fb6087
Kevin Durrenberger <KBerger1101@users.noreply.github.com>
2017-07-27 21:30:38 -0400

    Initial Push

---
+++ 4ChanImageScraper.py
@@ -0,0 +1,44 @@
+#Mess around
+
+from bs4 import BeautifulSoup, SoupStrainer
+
+import requests
+import re
+import urllib
+import shutil
+
+
+url = "4chan.org/co/"
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+soup = BeautifulSoup(data,'html.parser')
+
+
+#pick out all of the image file from the html code
+images = soup.find_all(class_='fileThumb')
+imageCount=0
+#go through them individually
+for image in images:
+    
+    print(imageCount)
+    urlimage= str(image)
+    match=re.findall( r'href="([^"]*)"' ,urlimage)
+    urlimage = match[0]
+    
+   
+    
+    print(urlimage)
+    imageCount+=1
+    imageSiteUrl= "http:" +urlimage
+    response = requests.get(imageSiteUrl, stream=True)
+    with open('img'+str(imageCount)+'.png','wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    
+
+
+
+
