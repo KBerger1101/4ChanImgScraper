9767198bb59c61633fd80e05ca66d27d6a1cba4b
Kevin Durrenberger <kevindurrenberger@gmail.com>
2017-09-08 19:04:42 -0400

    Added ability to request specific board

--- 4ChanImageScraper.py
+++ 4ChanImageScraper.py
@@ -7,10 +7,12 @@ import re
 import urllib
 import shutil
 
-
-url = "4chan.org/v/"
-
+#request board
+url = "4chan.org/"
+board= raw_input("What board do you want to scrape?")
+url= url+board+"/"
 r  = requests.get("http://" +url)
+
 
 data = r.text
 
@@ -22,7 +24,6 @@ images = soup.find_all(class_='fileThumb')
 imageCount=0
 #go through them individually
 for image in images:
-    
     print(imageCount)
     urlimage= str(image)
     match=re.findall( r'href="([^"]*)"' ,urlimage)
@@ -31,10 +32,11 @@ for image in images:
         
         urlimage = match[0] 
         print(urlimage)
-        
+        imageTitle= urlimage[-4:]
+        print(imageTitle)
         imageSiteUrl= "http:" +urlimage
         response = requests.get(imageSiteUrl, stream=True)
-        with open('img'+str(imageCount)+'.png','wb') as out_file:
+        with open('img'+str(imageCount)+imageTitle,'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
         del response
     imageCount+=1
